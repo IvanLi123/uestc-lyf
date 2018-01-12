bf276940ea438d8e019b0cc03f986d9b8a992c9d
IvanLi123 <767533763@qq.com>
2018-01-12 15:27:19 +0800

    clear

--- CS61b/src/Lab6/BadTransactionException.java
+++
@@ -1,9 +0,0 @@
-package Lab6;
-
-public class BadTransactionException extends Exception {
-int transactenumber;
-public BadTransactionException(int n){
-	super("the bad transactenumber is "+n);
-	this.transactenumber=n;
-}
-}
--- CS61b/src/sortedlist/Keyable.java
+++
@@ -1,6 +0,0 @@
-package sortedlist;
-
-public interface Keyable {
-    public int getKey();
-    public boolean lessThan(Keyable x);
-}
